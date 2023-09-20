3b629f53212e5e613de83ed749f390556d197a6d
Guilherme Gabriel Restio <125682298+Guuil4@users.noreply.github.com>
2023-09-20 13:22:28 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var currentDate = new Date();
+  var formattedDate = currentDate.toISOString(); // Format the date as an ISO string
+  res.end('Guilherme Gabriel Restio Current Date: ' + formattedDate);
+}).listen(8035);
